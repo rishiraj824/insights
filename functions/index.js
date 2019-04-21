d2db29de0f548778dcf7c61c0ed3e3e9f6d56c3a
Thiyagaraj T <thiyaga@Thiyagarajs-MacBook-Pro.local>
2019-04-21 13:14:55 +0530

    add fn for user

--- functions/index.js
+++ functions/index.js
@@ -22,11 +22,26 @@ const getDress = object => {
 		type: object.type,
 		name: object.name,
 		brand: object.brand,
+		itemCode: object.itemCode,
 		subType: object.subType,
 		userId: object.userId,
 		imagesURL: object.imagesURL,
 		likeability: object.likeability,
 		meta: object.meta
+	};
+};
+
+const getUser = object => {
+	return {
+		type: object.name,
+		age: object.age,
+		userId: object.id,
+		height: object.height,
+		braSize: object.braSize,
+		weight: object.weight,
+		bodyShape: object.bodyShape,
+		bellyShape: object.bellyShape,
+		buttShape: object.buttShape
 	};
 };
 
@@ -42,6 +57,24 @@ exports.addDress = functions.https.onRequest((request, response) => {
 		.database()
 		.ref("dress")
 		.push(getDress(data))
+		.then(snapshot => {
+			console.log(snapshot);
+			response.status(200).send("Added");
+		});
+});
+
+exports.addUser = functions.https.onRequest((request, response) => {
+	if (request.method !== "POST") {
+		response.status(400).send("Please send a POST request");
+		return;
+	}
+	const data = request.body;
+	console.log(data);
+
+	return admin
+		.database()
+		.ref("user")
+		.push(getUser(data))
 		.then(snapshot => {
 			console.log(snapshot);
 			response.status(200).send("Added");
