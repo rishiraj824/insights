0ab97441ca597c221ff0de2dbda86176314f5e42
Rishi Raj <rishiraj824@gmail.com>
2019-04-24 22:12:28 +0530

    ffixed the onboarding

--- src/Search/index.js
+++ src/Search/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Navbar from '../Dashboard/Navbar';
 
 class Search extends Component {
     render() {
         return (
             <div>
+                <Navbar />
                 <h3>Search</h3>
             </div>
         );
