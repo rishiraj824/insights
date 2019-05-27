4227f740dd34fb632cc38266bad615d56f656a4d
tstreamDOTh <tstream.h@outlook.com>
2019-05-28 00:10:53 +0530

    Update host

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
-    host: 'https://us-central1-prime-fit.cloudfunctions.net',
+    host: 'https://us-central1-hr-insights-c28c5.cloudfunctions.net',
     imageBB: {
         host:'https://api.imgbb.com/1/upload',
         apiKey: 'd04a9e42630abc6701082e56aa6129bb'
