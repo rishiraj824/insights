9f0057d3b758e2389c55ce0091a8cf49e0f6a9f9
tstreamDOTh <tstream.h@outlook.com>
2019-06-07 02:59:05 +0530

    Remove cors

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,6 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
 admin.initializeApp(functions.config().firebase);
-const cors = require("cors")({
-	origin: true
-});
 
 const firefunctions = new FireFunctionsFactory(admin.database());
 
