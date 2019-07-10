872ca14b0b923d55104e7c7212499d3eab154aec
tstreamDOTh <tstream.h@outlook.com>
2019-07-10 23:56:50 +0530

    Add bindings for report

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
 const FireFunctionsFactory = require("./lib");
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const cors = require("cors")({
+	origin: true
+});
 
 admin.initializeApp(functions.config().firebase);
 
@@ -13,7 +16,8 @@ exports.addApplicant = firefunctions.getFunction("CREATE", {
 		experience: object.experience,
 		age: object.age,
 		role: object.role,
-		transcript: object.transcript ? object.transcript : 'UNAVAILABLE'
+		transcript: object.transcript ? object.transcript : "UNAVAILABLE",
+		report: object.report ? object.report : []
 	})
 });
 
@@ -34,6 +38,58 @@ exports.updateApplicant = firefunctions.getFunction("PATCH", {
 		experience: object.experience,
 		age: object.age,
 		role: object.role,
-		transcript: object.transcript
+		transcript: object.transcript,
+		report: object.report ? object.report : []
 	})
+});
+
+exports.getDummyReport = functions.https.onRequest((request, response) => {
+	cors(request, response, () => {
+		if (request.method !== "POST") {
+			response.status(400).send("Please send a POST request");
+			return;
+		}
+
+		response.status(200).send(
+			JSON.stringify({
+				radarCharts: [
+					{
+						title: "Trends",
+						data: [
+							{ word: "Blockchain", A: 150, fullMark: 150 },
+							{ word: "Cyrpto", A: 150, fullMark: 150 },
+							{ word: "IOT", A: 86, fullMark: 150 },
+							{ word: "Security", A: 99, fullMark: 150 },
+							{ word: "Automation", A: 85, fullMark: 150 }
+						]
+					}
+				],
+
+				ratioBarCharts: [
+					{
+						title: "I vs We",
+						data: [{ label: "I/Me", percent: 10, val: 5 }, { label: "We/Us", percent: 90, val: 45 }]
+					},
+					{
+						title: "Postive vs Negative",
+						data: [{ label: "Positive", percent: 40, val: 100 }, { label: "Negative", percent: 60, val: 400 }]
+					}
+				],
+				textCharts: [
+					{
+						title: "Words similar to Team",
+						data: "7232 Mentions"
+					},
+					{
+						title: "Questions Asked",
+						data: "7 Questions"
+					},
+					{
+						title: "Words similar to Innovation",
+						data: "23 Mentions"
+					}
+				]
+			})
+		);
+	});
 });
