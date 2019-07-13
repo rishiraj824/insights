d965dff495c93fd7549a757596377aa798d72cdb
Rishi Raj <rishiraj824@gmail.com>
2019-07-14 01:27:28 +0530

    added sort filter and workspace APIs

--- src/Onboarding/index.js
+++ src/Onboarding/index.js
@@ -1,24 +1,24 @@
-import React, { Component, PropTypes } from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { onChange, addApplicant } from "../store/actions/onboarding";
 import ReactTable from 'react-table';
 import '../../node_modules/react-table/react-table.css'
 import Modal from "react-modal";
-import { getApplicants } from "../store/actions/applicants";
+import { getApplicants, onFilteredChange, filterAllFunction, roleFilter, filterAllMobFunction } from "../store/actions/applicants";
 import {isMobile} from 'react-device-detect';
 import '../Mobile.css';
 import '../Table.css';
+import matchSorter from 'match-sorter'
 
 class Onboarding extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			isShowing: false,
-			data: [],
 			loading: true
 		};
 	}
-
+	
 	componentDidMount() {
 		const { getApplicants } = this.props;
 		getApplicants();
@@ -43,9 +43,6 @@ class Onboarding extends Component {
 	  if (rowInfo) {
 		return {
 			onClick: ()=> {
-				console.log(rowInfo.original);
-				console.log(this.context);
-				//this.context.history.push(`/applicant/interview/${rowInfo.original.id}`)
 				window.location.href = `/applicant/${rowInfo.original.id}/interview`
 			}
 		}
@@ -62,13 +59,11 @@ class Onboarding extends Component {
 			}
 		});
 	};
-	
-
 	render() {
 		const { isShowing, loading } = this.state;
-		const { addApplicant, values } = this.props;
-		const { applicants: data } = this.props;
-		  const columns = [{
+		const { addApplicant, values, role, onFilteredChange, roleFilter, filtered, filterAllMobFunction, filterAll, filterAllFunction } = this.props;
+		const { applicants: data, original } = this.props;
+		const columns = [{
 			Header: 'Name',
 			accessor: 'name'
 		  }, {
@@ -81,15 +76,47 @@ class Onboarding extends Component {
 			},{
 			Header: 'Age',
 			accessor: 'age'
+		  },{
+              Header: "All",
+              id: 'all',
+              width: 0,
+              resizable: false,
+              sortable: false,
+              Filter: () => { },
+              getProps: () => {
+                return {
+                  // style: { padding: "0px"}
+                }
+			  },
+			  filterMethod: (filter, rows) => {
+                const result = matchSorter(rows, filter.value, {
+                  keys: [
+                    "name",
+					"age",
+					"role",
+					"experience"
+                  ], threshold: matchSorter.rankings.WORD_STARTS_WITH
+                });
+                return result;
+			  },			  
+              filterAll: true,
 		  }]
+		const rolesSet = new Set;
+		original.map(applicant=>{		 
+			rolesSet.add(applicant.role);
+		});
+		const roles = Array.from(rolesSet);
+		const options = roles.map((role, i)=>{
+			return <option key={i+role} value={role}>{role}</option>						
+		});
 		return (
 				<div className="flex row wrap dashboard space-evenly">
 				{isMobile?<div style={{width: '100%'}}>
 					<div className='mobileFilters'>				
-						<input className='mobileSearch' placeholder="Search" />
-						<select className='mobileSelect' value='analyst'>
-							<option value={'analyst'}>Analyst</option>
-							<option value={'dev'}>Dev</option>
+						<input value={filterAll} onChange={filterAllMobFunction} className='mobileSearch' placeholder="Search" />
+						<select onChange={(e)=>roleFilter(e.target.value)} className='mobileSelect' value={role}>
+							<option key={"0all"} value={'all'}>Show All ({original.length})</option>
+							{options}
 						</select>
 					</div>
 					<h3 className="addition mobileAddition" onClick={this.openModalHandler}>+ Add Applicant</h3>
@@ -122,12 +149,12 @@ class Onboarding extends Component {
 					</div>:<>
 					<div className="filters">
 				<h3 className="filterHeader">Show</h3>
-					<div className="filterMain">All Applicants</div>
-					<div className="filter">Analyst</div>
+					<div onClick={roleFilter.bind(null, 'all')} className={role==='all'?"filterMain":"filter"}>All Applicants ({original.length})</div>
+					{roles.map((roley,ind)=> <div key={ind} onClick={roleFilter.bind(null, roley)} className={role===roley?"filterMain":"filter"}>{roley}</div>)}
 				</div>
 				<div className="table">
 					<div className="actions">
-						<input placeholder="Search" />
+						<input value={filterAll} onChange={filterAllFunction} placeholder="Search" />
 						<h3 className="addition" onClick={this.openModalHandler}>+ Add Applicant</h3>
 					</div>
 					<ReactTable
@@ -135,6 +162,10 @@ class Onboarding extends Component {
 						columns={columns} 
 						loading={loading}
 						sortable={true}
+          				defaultFilterMethod={(filter, row) =>
+            				String(row[filter.id]) === filter.value}
+						onFilteredChange={onFilteredChange}
+						filtered={filtered}
 						multiSort={true}
 						showPagination={false}
 						showPageSizeOptions={false}
@@ -170,8 +201,12 @@ const mapStateToProps = state => {
 		profile: state.firebase.profile,
 		auth: state.firebase.auth,
 		values: state.onboarding.values,
-		applicants: state.applicants,
-		history: state.history
+		applicants: state.applicants.data,
+		history: state.history,
+		filterAll: state.applicants.filterAll,
+		filtered: state.applicants.filtered,
+		role: state.applicants.role,
+		original: state.applicants.original
 	};
 };
 
@@ -180,7 +215,11 @@ const mapDispatchToProps = dispatch => {
 	return {
 		onChange: payload => dispatch(onChange(payload)),
 		addApplicant: payload => dispatch(addApplicant(payload)),
-		getApplicants: payload => dispatch(getApplicants(payload))
+		getApplicants: payload => dispatch(getApplicants(payload)),
+		filterAllFunction: payload => dispatch(filterAllFunction(payload)),
+		filterAllMobFunction: payload => dispatch(filterAllMobFunction(payload)),
+		roleFilter: payload => dispatch(roleFilter(payload)),
+		onFilteredChange: (filtered, filterAllState) => dispatch(onFilteredChange(filtered, filterAllState))
 	};
 };
 
--- src/store/reducers/applicants.js
+++ src/store/reducers/applicants.js
@@ -1,9 +1,48 @@
-const applicants = (state = [], action) => {
+import matchSorter from 'match-sorter';
+
+const applicants = (state = { original: [], data: [], filterAll:'', filtered: [], role:'all' }, action) => {
     switch (action.type) {
         case 'APPLICANTS_FETCHED':
 			const data = Object.keys(action.payload).map(key=> ({...action.payload[key], id: key}))
 
-            return data            
+            return {
+                ...state,
+                data,
+                original: data            
+            }
+        case 'ROLE_CHANGE_MOBILE':
+            const result = matchSorter(state.original, action.payload.filterAll, {
+                  keys: [
+                    "name",
+					"age",
+					"role",
+					"experience"
+                  ], threshold: matchSorter.rankings.WORD_STARTS_WITH
+                });
+            return {
+                ...state,
+                data: result,
+                filterAll: action.payload.filterAll
+            }
+        case 'ROLE_CHANGE':
+            if(action.payload.role==='all'){
+                return {
+                    ...state,
+                    data: state.original,
+                    role: 'all'
+                }
+            }
+            return {
+                ...state,
+                role: action.payload.role,
+                data:state.original.filter(applicant=>applicant.role === action.payload.role)
+            }
+        case 'FILTER_ALL':
+        case 'ON_FILTER_CHANGE':
+            return {
+                ...state,
+                ...action.payload
+            }
         default: 
             return state    
     }
