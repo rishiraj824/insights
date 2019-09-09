0275607eb853bf59a00a60179aacefe941ac84ac
tstreamDOTh <tstream.h@outlook.com>
2019-09-10 01:40:51 +0530

    On click of logo redirect to home

--- src/Recorder.js
+++ src/Recorder.js
@@ -1,82 +1,92 @@
 import React, { Component } from 'react';
 import './Recorder.css';
 import { getApplicant, updateTranscript } from './store/actions/applicants';
-import {isMobile} from 'react-device-detect';
-import { connect } from "react-redux";
+import { isMobile } from 'react-device-detect';
+import { connect } from 'react-redux';
 import questions from './questions';
 import Dictaphone from './SpeechButton';
 
 class Recorder extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            question: 'Tell me about a time when you were faced with a problem that had a number of possible solutions. What was the problem and how did you determine the course of action? What was the outcome of that choice?'
-        }
-    }
-    componentDidMount() {
-        const { getApplicant } = this.props;
-        getApplicant(this.props.match.params.id)
-    }
-    changeQuestion = () => {
-        this.setState({
-            question: questions[Math.round(Math.random() * (questions.length - 0))].question
-        })
-    }
-    render() {
-        //const applicant = {"age":22,"experience":2,"name":"Benjamin Tod","role":"Assistant Manager"};
-        const { auth, applicant, updateTranscript } = this.props;
-        const id = this.props.match.params.id;
-        const { question } = this.state;
-        return (
-            <>
-            {auth.uid? <div className="logoNav">
-            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="31" viewBox="0 0 26 31">
-                        <g fill="none" fillRule="evenodd" stroke="#000" strokeWidth="1.5" transform="translate(1 1)">
-                            <path d="M0 28.458V9.253c.505-7.783 16.926-16.194 21.979 0 0 .162.625 2.884 1.874 8.167a.505.505 0 0 1-.492.621h-1.74a.505.505 0 0 0-.505.525c.062 1.659.055 2.488-.021 2.488-5.053-.878-6.19 4.269 0 4.269v3.135a.505.505 0 0 1-.506.505H.505A.505.505 0 0 1 0 28.458z"/>
-                            <ellipse cx="13.642" cy="12.554" rx="2.021" ry="2.009"/>
-                        </g>
-                    </svg>
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                {isMobile? '':'Coculture'}
-            </div>:''}
-            <div className='recorder'>            
-                <div className='recorderTop space-between flex wrap center'>                
-                       <div className='details'>
-                            <h4 className='name'>{applicant.name}</h4>
-                            <h4 className='role'>{applicant.role}</h4>
-                            <h4 className='age'>{applicant.age}</h4>
-                       </div> 
-                        <div className='mic'>
-                            <Dictaphone updateTranscript={updateTranscript} id={id} applicant={applicant}  />
-                        </div>
-                </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      question:
+        'Tell me about a time when you were faced with a problem that had a number of possible solutions. What was the problem and how did you determine the course of action? What was the outcome of that choice?'
+    };
+  }
+  componentDidMount() {
+    const { getApplicant } = this.props;
+    getApplicant(this.props.match.params.id);
+  }
+  changeQuestion = () => {
+    this.setState({
+      question: questions[Math.round(Math.random() * (questions.length - 0))].question
+    });
+  };
+  render() {
+    //const applicant = {"age":22,"experience":2,"name":"Benjamin Tod","role":"Assistant Manager"};
+    const { auth, applicant, updateTranscript } = this.props;
+    const id = this.props.match.params.id;
+    const { question } = this.state;
+    return (
+      <>
+        {auth.uid ? (
+          <a href='/' className='logoNav'>
+            <svg xmlns='http://www.w3.org/2000/svg' width='26' height='31' viewBox='0 0 26 31'>
+              <g fill='none' fillRule='evenodd' stroke='#000' strokeWidth='1.5' transform='translate(1 1)'>
+                <path d='M0 28.458V9.253c.505-7.783 16.926-16.194 21.979 0 0 .162.625 2.884 1.874 8.167a.505.505 0 0 1-.492.621h-1.74a.505.505 0 0 0-.505.525c.062 1.659.055 2.488-.021 2.488-5.053-.878-6.19 4.269 0 4.269v3.135a.505.505 0 0 1-.506.505H.505A.505.505 0 0 1 0 28.458z' />
+                <ellipse cx='13.642' cy='12.554' rx='2.021' ry='2.009' />
+              </g>
+            </svg>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            {isMobile ? '' : 'Coculture'}
+          </a>
+        ) : (
+          ''
+        )}
 
-                <div className='question-sets wrap flex center space-between'>       
-                    <div className='question'><h3>Question Ideas</h3>{question}</div>
-                    <button className='width-120' onClick={this.changeQuestion}>Show Another</button>
-                </div>
+        <div className='recorder'>
+          <div className='recorderTop space-between flex wrap center'>
+            <div className='details'>
+              <h4 className='name'>{applicant.name}</h4>
+              <h4 className='role'>{applicant.role}</h4>
+              <h4 className='age'>{applicant.age}</h4>
             </div>
-            </>
-        );
-    }
+            <div className='mic'>
+              <Dictaphone updateTranscript={updateTranscript} id={id} applicant={applicant} />
+            </div>
+          </div>
+
+          <div className='question-sets wrap flex center space-between'>
+            <div className='question'>
+              <h3>Question Ideas</h3>
+              {question}
+            </div>
+            <button className='width-120' onClick={this.changeQuestion}>
+              Show Another
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
-
 const mapStateToProps = state => {
-	return {
-		auth: state.firebase.auth,
-        applicant: state.applicant
-	};
+  return {
+    auth: state.firebase.auth,
+    applicant: state.applicant
+  };
 };
 
 const mapDispatchToProps = dispatch => {
-	return {
-        getApplicant: payload => dispatch(getApplicant(payload)),
-        updateTranscript: payload => dispatch(updateTranscript(payload)),
-	};
+  return {
+    getApplicant: payload => dispatch(getApplicant(payload)),
+    updateTranscript: payload => dispatch(updateTranscript(payload))
+  };
 };
 
 export default connect(
-	mapStateToProps,
-	mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Recorder);
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,25 +1,35 @@
-import React from "react";
-import { isMobile } from "react-device-detect";
+import React from 'react';
+import { isMobile } from 'react-device-detect';
 
 export const getNavbar = (auth, signOut, signOutDialog, showDialog) => {
-	return auth.uid ? (
-		<div className="logoNav">
-			<svg xmlns="http://www.w3.org/2000/svg" width="26" height="31" viewBox="0 0 26 31">
-				<g fill="none" fillRule="evenodd" stroke="#000" strokeWidth="1.5" transform="translate(1 1)">
-					<path d="M0 28.458V9.253c.505-7.783 16.926-16.194 21.979 0 0 .162.625 2.884 1.874 8.167a.505.505 0 0 1-.492.621h-1.74a.505.505 0 0 0-.505.525c.062 1.659.055 2.488-.021 2.488-5.053-.878-6.19 4.269 0 4.269v3.135a.505.505 0 0 1-.506.505H.505A.505.505 0 0 1 0 28.458z" />
-					<ellipse cx="13.642" cy="12.554" rx="2.021" ry="2.009" />
-				</g>
-			</svg>
-			&nbsp;&nbsp;&nbsp;&nbsp;
-			{isMobile ? "" : "Coculture"}			
-			{isMobile? '':<a className='link logout' onClick={signOutDialog}>
-				{auth.displayName} &nbsp;<i className='fa fa-chevron-down'></i>
-			</a>}
-			{showDialog? <div className="dialog">
-				<a className='link' onClick={signOut}>Logout</a>
-				</div> :''}
-		</div>
-	) : (
-		<span />
-	);
+  return auth.uid ? (
+    <a href='/' className='logoNav'>
+      <svg xmlns='http://www.w3.org/2000/svg' width='26' height='31' viewBox='0 0 26 31'>
+        <g fill='none' fillRule='evenodd' stroke='#000' strokeWidth='1.5' transform='translate(1 1)'>
+          <path d='M0 28.458V9.253c.505-7.783 16.926-16.194 21.979 0 0 .162.625 2.884 1.874 8.167a.505.505 0 0 1-.492.621h-1.74a.505.505 0 0 0-.505.525c.062 1.659.055 2.488-.021 2.488-5.053-.878-6.19 4.269 0 4.269v3.135a.505.505 0 0 1-.506.505H.505A.505.505 0 0 1 0 28.458z' />
+          <ellipse cx='13.642' cy='12.554' rx='2.021' ry='2.009' />
+        </g>
+      </svg>
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      {isMobile ? '' : 'Coculture'}
+      {isMobile ? (
+        ''
+      ) : (
+        <a className='link logout' onClick={signOutDialog}>
+          {auth.displayName} &nbsp;<i className='fa fa-chevron-down'></i>
+        </a>
+      )}
+      {showDialog ? (
+        <div className='dialog'>
+          <a className='link' onClick={signOut}>
+            Logout
+          </a>
+        </div>
+      ) : (
+        ''
+      )}
+    </a>
+  ) : (
+    <span />
+  );
 };
